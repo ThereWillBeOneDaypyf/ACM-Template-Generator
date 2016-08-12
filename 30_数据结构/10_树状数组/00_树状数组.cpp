c87a8300837e9abba6701335235a220b8bebd3a1
mangogao <jjmangogao@gmail.com>
2016-08-12 12:16:50 +0800

    update
    update

--- 30_数据结构/10_树状数组/00_树状数组.cpp
+++ 30_数据结构/10_树状数组/00_树状数组.cpp
@@ -1,20 +1,19 @@
 // ---
 // $O(\log n)$查询和修改数组的前缀和
 // ---
-// 注意下标应从1开始
-#define lowbit(i) (i&(-i))
-int bit[maxn], n; 
-int query(int i){
+// 注意下标应从1开始 n是全局变量
+int bit[N], n; 
+int sum(int i){
 	int s = 0;
 	while(i){
 		s += bit[i];
-		i -= lowbit(i);
+		i -= i&-i;
 	}
 	return s;
 }
 void add(int i, int x){
 	while(i<=n){
 		bit[i] += x;
-		i += lowbit(i);
+		i += i&-i;
 	}
 }
